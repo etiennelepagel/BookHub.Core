70ed3de21dbfc8733e6b3ef4e457ada0d94af116
etiennelepagel <etiennelepagel@gmail.com>
2023-10-17 23:10:25 -0400

    add firestore attribute

--- BookHub.Core/BookHub.Core.Persistence/Entities/Business.cs
+++ BookHub.Core/BookHub.Core.Persistence/Entities/Business.cs
@@ -5,19 +5,48 @@ namespace BookHub.Core.Persistence.Entities;
 [FirestoreData]
 public class Business
 {
+    [FirestoreDocumentId]
     public string Id { get; set; }
+    
+    [FirestoreProperty]
     public string Address { get; set; }
+    
+    [FirestoreProperty]
     public string BannerImageLink { get; set; }
+    
+    [FirestoreProperty]
     public string BusinessId { get; set; }
+    
+    [FirestoreProperty]
     public string BusinessName { get; set; }
+    
+    [FirestoreProperty]
     public string Category { get; set; }
+    
+    [FirestoreProperty]
     public string CloseTime { get; set; }
+    
+    [FirestoreProperty]
     public string Description { get; set; }
+    
+    [FirestoreProperty]
     public string[] Facilities { get; set; }
+    
+    [FirestoreProperty]
     public string FbLink { get; set; }
+    
+    [FirestoreProperty]
     public string InstaLink { get; set; }
+    
+    [FirestoreProperty]
     public string LogoImageLink { get; set; }
+    
+    [FirestoreProperty]
     public string OpenTime { get; set; }
+    
+    [FirestoreProperty]
     public string PhoneNo { get; set; }
+    
+    [FirestoreProperty]
     public string XLink { get; set; }
 }
