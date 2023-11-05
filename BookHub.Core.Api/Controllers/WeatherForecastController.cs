53d7ebdf012256b9b753aba22610be8677dbf36f
Etienne <etiennelepagel@gmail.com>
2023-11-05 08:48:10 -0500

    move files

--- BookHub.Core.Api/Controllers/WeatherForecastController.cs
+++ BookHub.Core.Api/Controllers/WeatherForecastController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace WebApplication1.Controllers
+namespace BookHub.Core.Api.Controllers
 {
     [ApiController]
     [Route("[controller]")]
--- BookHub.Core.Api/WeatherForecast.cs
+++ BookHub.Core.Api/WeatherForecast.cs
@@ -1,4 +1,4 @@
-namespace WebApplication1
+namespace BookHub.Core.Api
 {
     public class WeatherForecast
     {
